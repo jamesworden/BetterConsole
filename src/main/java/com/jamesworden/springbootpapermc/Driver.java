b2a9d3b9daad6742005f2db4bcecc4811431c73f
James Worden <jameswilli4m@gmail.com>
2020-09-06 20:14:33 -0400

    Laying core infrastructure

--- src/main/java/com/jamesworden/springbootpapermc/Driver.java
+++ src/main/java/com/jamesworden/springbootpapermc/Driver.java
@@ -1,27 +1,54 @@
 package com.jamesworden.springbootpapermc;
 
+import com.jamesworden.springbootpapermc.minecraft.Configuration;
+import com.jamesworden.springbootpapermc.minecraft.GameCommand;
+import com.sun.javafx.util.Logging;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.context.logging.LoggingApplicationListener;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.util.Collections;
+import java.util.Objects;
+
+/**
+ * Main plugin class for BetterConsole
+ *
+ * @author James Worden
+ */
 public final class Driver extends JavaPlugin {
 
 	private ConfigurableApplicationContext context;
-
-	@Override
-	public void onLoad() {
-		context = SpringApplication.run(Application.class);
-	}
+	private Configuration configuration;
 
 	@Override
 	public void onEnable() {
-		// Plugin startup logic
+
+		// Load configuration
+		saveDefaultConfig();
+		configuration = Configuration.getInstance();
+		configuration.init(getConfig());
+
+		// Register command
+		Objects.requireNonNull(getCommand("betterconsole")).setExecutor(new GameCommand());
+
+		// Configure server port and properties
+		SpringApplication app = new SpringApplication(Application.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", configuration.getPort()));
+		app.setBannerMode(Banner.Mode.OFF);
+
+		// Start the server
+		context = app.run();
 		context.start();
 	}
 
 	@Override
 	public void onDisable() {
-		// Plugin shutdown logic
-		context.close();
+
+		// Stop the server
+		if (context != null) context.close();
 	}
+
 }
